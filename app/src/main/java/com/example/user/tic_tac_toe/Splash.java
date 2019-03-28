43386ad05e9433b9f465b38777e90b24eb773fe0
sadia-jannat <sadiatonni1995@gmail.com>
2019-03-28 09:16:22 +0600

    first project.

---
+++ app/src/main/java/com/example/user/tic_tac_toe/Splash.java
@@ -0,0 +1,30 @@
+package com.example.user.tic_tac_toe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread t=new Thread()
+        {
+            public  void run()
+            {
+                try{
+                    sleep(3000);
+                    Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
+    }
+}
